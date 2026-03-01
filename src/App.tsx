e7af1721187f66eeff1ebd55da18e9bc5b4fa7b6
sarthakroutray <sarthak.routray2006@gmail.com>
2026-03-01 13:15:48 +0530

    Rollback

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import { useState } from 'react';
-import Preloader from './components/Preloader';
-
-export default function App() {
-    const [loading, setLoading] = useState(true);
-
+export default function App(){
     return (
-        <>
-            {loading && <Preloader onComplete={() => setLoading(false)} />}
-            {!loading && (
-                <h1>Oneiros-26 Application Loading Complete</h1>
-            )}
-        </>
+        <h1>App</h1>
     )
 }
